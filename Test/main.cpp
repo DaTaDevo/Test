ffda76f681bcda79989326823c8771f8195dc377
DaTaDevo <denixden2818@gmail.com>
2019-08-31 18:32:30 +0300

    Add Mlist like a container

--- Test/main.cpp
+++ Test/main.cpp
@@ -10,5 +10,12 @@ int main ()
     m.addPeople("Name_1",23,"girl");
     std::string str = m.findPeople("Name").toString();
     cout <<str<< endl;
+    Mlist array(4);
+    array[0] = 1.0;
+    array[1] = 2.0;
+    array[2] = 3.0;
+    array[3] = 4.0;
+    double *item = array.begin();
+    cout <<*item<< endl;
     return 0;
 }
--- Test/model.h
+++ Test/model.h
@@ -5,6 +5,112 @@
 #include <stdlib.h>
 #include <string>
 
+
+class Mlist //свой контейнер M - my или model (если будет использовать для модели ) list
+{
+    int length = 0;
+    double* array = new double[length];
+public:
+    Mlist()
+    {
+
+    }
+    Mlist(int size) : length(size)
+    {
+
+    }
+    ~Mlist()
+    {
+        delete [] array;
+    }
+    void setlength(int size)
+    {
+        this->length = size;
+    }
+    int size()
+    {
+        return this->length;
+    }
+    void push_back( double item)
+    {
+        length++;
+        array[length-1] = item;
+    }
+    void pop_back()
+    {
+        if (length-1 <= 0)//проверка на то будет ли массив пустым после выполнений функции
+        {
+            this->array = nullptr;
+            length = 0;
+            this->array = new double[length];
+            return;
+        }
+            length --;
+            double temp_array[this->length];
+            for (int i = 0;i<length;i++)
+            {
+                temp_array[i] = this->array[i];
+            }
+
+            this->array = nullptr;  //читим массив дял присовоенния новых данных
+            this->array = new double [length];
+            for (int i = 0; i < length; ++i)
+            {
+                this->array[i] = temp_array[i];
+            }
+    }
+    void push_front(double item)
+    {
+        length++;
+        if (length-1 == 0)//проверка был ли массив пустым до выпонения этой функции
+        {
+            push_back(item);
+            return;
+        }
+        for (int i = length-1;i>0;i--)
+        {
+            this->array[i] = this->array[i-1];
+        }
+        this->array[0] = item;
+    }
+    void pop_front()
+    {
+        if(length - 1 == 0)
+        {
+            this->array = nullptr;
+            length--;
+            this->array = new double[length];
+            return;
+        }
+        else if (length - 1 < 0)
+            return ;
+
+        length --;
+        double temp_array[length];
+        for (int i = 0; i < length ;i++)
+        {
+            temp_array[i] = this->array[i+1];
+        }
+        //очистка array
+        this->array = nullptr;
+        this->array = new double[length];
+        for (int i  = 0; i < length;i++)
+        this->array[i] = temp_array[i];
+    }
+    double* begin()
+    {
+        return &array[0];
+    }
+    double* end ()
+    {
+        return &array[length-1];
+    }
+    double& operator [](const int index)
+    {
+        return array[index];
+    }
+};
+
 class Model;
 
 class AbstractModel
